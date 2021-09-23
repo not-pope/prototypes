cb58d520b4ea249c25067cc8c2572dfddea9f510
Luka Ivanovic <ivanovic@tutanota.de>
2021-09-23 18:27:35 +0200

    c3 (yikes....)

--- main-area/script.js
+++ main-area/script.js
@@ -1,11 +1,13 @@
 let mainArea = document.getElementById("main-area");
 let notes = document.getElementById("notes");
+let text = document.getElementById("input");
 
 let i = 0;
 
 let addNew = () => {
     let note = document.createElement("div");
     note.classList.add("note");
-    note.innerText=(++i);
-    notes.appendChild(note);
+    note.innerText=text.value;
+    // notes.appendChild(note);
+    notes.prepend(note);
 }
